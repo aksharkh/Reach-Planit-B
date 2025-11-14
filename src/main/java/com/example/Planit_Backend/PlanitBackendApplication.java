3081b1ac6cd1de016ce29366ad6d352e118e948f
aksharkh <aksharkh04@gmail.com>
2025-11-14 18:32:27 +0530

    Initialized project

---
+++ src/main/java/com/example/Planit_Backend/PlanitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Planit_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Planit_Backend/PlanitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Planit_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
